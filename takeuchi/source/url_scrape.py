b9922a700b8d907c938fc26247589b455aafaf09
kenty <tktkdx.62000@gmail.com>
2020-03-16 10:20:19 +0900

    scrape url

---
+++ takeuchi/source/url_scrape.py
@@ -0,0 +1,25 @@
+import requests
+import re
+
+from bs4 import BeautifulSoup
+
+r = requests.get("https://www.pixiv.net/ranking.php?mode=daily")
+path = "../data/artworks.txt"
+
+#出力が絶対パスの場合はコメント後で消す。
+head = "https://www.pixiv.net"
+
+#第一引数:解析したいもの, 第二引数:何を元に解析するか(htmlなのでhtml.parser)
+soup = BeautifulSoup(r.content,"html.parser")
+
+#htmlファイル中のurlの情報を全て抽出
+links = [url.get('href') for url in soup.find_all('a')]
+
+#"artworks"を含むurlを抽出
+links_in = [s for s in links if 'artworks' in s]
+
+#artworks.txtにurlを一行ずつ記入
+with open(path,'wt') as f:
+	for ele in links_in:
+		f.write(head+ele+'\n')
+
